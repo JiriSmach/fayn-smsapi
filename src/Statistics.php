2fd6bb907bb8ab9dea96e7e18cec6caed1e7c3be
JiriSmach <54078880+JiriSmach@users.noreply.github.com>
2023-01-14 14:42:43 +0100

    Update Statistics.php

--- src/Statistics.php
+++ src/Statistics.php
@@ -1,5 +1,14 @@
-<?php
+<?php declare(strict_types=1);
+
+namespace JiriSmach\FaynSmsApi;
 
 class Statistics
 {
+    private Connection $connection;
+
+    public funxtion __construct(
+        Connection $connection
+    ) {
+        $this->connection = $connection;
+    }
 }
