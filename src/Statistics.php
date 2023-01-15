8a8b95119dff3971040a1a52269c3106e2db410f
JiriSmach <54078880+JiriSmach@users.noreply.github.com>
2023-01-15 09:37:49 +0100

    Update Statistics.php

--- src/Statistics.php
+++ src/Statistics.php
@@ -17,18 +17,18 @@ class Statistics
     public function getStatistics(?DateTimeInterface $from = null, ?DateTimeInterface $to = null, ?int $userId = null): array
     {
         $method = 'statistics';
+        $statisticsRequest = new StatisticsRequest();
         if ($userId) {
             $method .= '/' . $userId;
         }
-        $params = [];
         if ($from) {
-            $params['creditHistoryFrom'] = $from->format(DateTimeInterface::ATOM);
+            $statisticsRequest->addParam('creditHistoryFrom', $from->format(DateTimeInterface::ATOM));
         }
         if ($to) {
-            $params['creditHistoryTo'] = $to->format(DateTimeInterface::ATOM);
+            $statisticsRequest->addParams('creditHistoryTo', $to->format(DateTimeInterface::ATOM));
         }
 
-        $this->connection->getRequest($method, $params);
+        $this->connection->getRequest($method, $statisticsRequest);
         // todo: dostat z odpovedi data a vytvorit tridu
         return [];
     }
