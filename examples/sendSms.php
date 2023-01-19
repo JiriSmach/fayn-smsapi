642fd6bc70aa4ea57a8f58881baf0e1fcc1752e9
JiriSmach <54078880+JiriSmach@users.noreply.github.com>
2023-01-19 20:57:44 +0100

    Update sendSms.php

--- examples/sendSms.php
+++ examples/sendSms.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $connection = new \JiriSmach\FaynSmsApi\Connection('username', 'password');
 
-$smsWrapper = new \JiriSmach\FaynSmsApi\SmsWrapper();
+$smsWrapper = new \JiriSmach\FaynSmsApi\Wrappers\SmsWrapper();
 $smsWrapper->setNumber('777777777');
 $smsWrapper->setText('Test text');
 
